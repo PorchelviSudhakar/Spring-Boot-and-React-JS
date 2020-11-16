314759743b9319fcbf58c6d0e39c47a9b5646e2f
PorchelviSudhakar <porchelvigunasekaran@gmail.com>
2020-11-16 11:40:13 +0100

    Spring Boot Application entry point

---
+++ sampleproject/src/main/java/com/example/sampleproject/ReactspringbootprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactspringbootprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactspringbootprojectApplication.class, args);
+	}
+
+}
